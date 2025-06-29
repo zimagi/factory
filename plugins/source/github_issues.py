9ff8e4863fc0e64c274eb11c599dd602f20d0b6e
Adrian Webb <adrian@webb.sh>
2025-06-29 05:55:27 -0400

    Uncommenting the saving of last time processed in the github issues source plugin provider.

--- plugins/source/github_issues.py
+++ plugins/source/github_issues.py
@@ -18,8 +18,8 @@ class Provider(BaseProvider("source", "github_issues")):
                     if not issue.pull_request:
                         yield issue
 
-        # if not self.field_disable_save:
-        #    self.command.set_state(state_key, current_time)
+        if not self.field_disable_save:
+            self.command.set_state(state_key, current_time)
 
     def load_item(self, issue, context):
         users = self.add_github_users(issue.user, issue.assignee, *issue.assignees)
